f499312c97525795620a4f2ac930380843d22e72
asif3062 <asifbsb2002@gmail.com>
2022-10-24 20:29:06 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARPncH2Mn5V_a-RyXzxOohQFBne6hxOjU",
+  authDomain: "binary-learning-3fa53.firebaseapp.com",
+  projectId: "binary-learning-3fa53",
+  storageBucket: "binary-learning-3fa53.appspot.com",
+  messagingSenderId: "434420555825",
+  appId: "1:434420555825:web:844e6b282e990b35c93510"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
