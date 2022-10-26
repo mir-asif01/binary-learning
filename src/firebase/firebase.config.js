9ee689a34c7bdc6ad0917d2955956eead12470d3
asif3062 <asifbsb2002@gmail.com>
2022-10-26 16:58:14 +0600

    environment variable file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,21 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARPncH2Mn5V_a-RyXzxOohQFBne6hxOjU",
-  authDomain: "binary-learning-3fa53.firebaseapp.com",
-  projectId: "binary-learning-3fa53",
-  storageBucket: "binary-learning-3fa53.appspot.com",
-  messagingSenderId: "434420555825",
-  appId: "1:434420555825:web:844e6b282e990b35c93510"
+  // apiKey: "AIzaSyARPncH2Mn5V_a-RyXzxOohQFBne6hxOjU",
+  // authDomain: "binary-learning-3fa53.firebaseapp.com",
+  // projectId: "binary-learning-3fa53",
+  // storageBucket: "binary-learning-3fa53.appspot.com",
+  // messagingSenderId: "434420555825",
+  // appId: "1:434420555825:web:844e6b282e990b35c93510"
+
+  // apiKey: "AIzaSyARPncH2Mn5V_a-RyXzxOohQFBne6hxOjU",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+
 };
 
 // Initialize Firebase
